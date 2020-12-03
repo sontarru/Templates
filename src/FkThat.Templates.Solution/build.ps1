b7cedc13c469691a972e6a0e75c8998ab3081466
FkThat <fkthat@yandex.ru>
2020-12-03 22:59:05 +0300

    Update build.ps1

--- src/FkThat.Templates.Solution/build.ps1
+++ src/FkThat.Templates.Solution/build.ps1
@@ -1,70 +1,36 @@
 [CmdletBinding()]
-param (
+param(
     [Parameter(Position = 0)]
-    [ValidateSet('restore', 'build', 'test', 'clean', 'pack', 'run', 'watch')]
+    [ValidateSet('clean', 'restore', 'build', 'test', 'coverage')]
     $Target = 'build',
-
     [Parameter()]
     [ValidateSet('Debug', 'Release')] 
-    $Config = 'Debug',
-
-    [Parameter()]
-    [Switch] 
-    $ShowCoverageReport,
-
-    [Parameter()]
-    [string]
-    $StartProject,
-
-    [Parameter()]
-    [string]
-    $LaunchProfile
+    $Config = 'Debug'
 )
-
-if($Target -eq 'run' || $Target -eq 'watch') {
-    if(-not $StartProject) {
-        Write-Error '-StartProject parameter required'
-        break
-    }
-
-    $StartProject = (Get-Item $StartProject).FullName
-}
 
 Push-Location $PSScriptRoot
 
-switch($Target) {
-    'clean' {
-        git clean -dfx -e .vs -e .vscode
-    }
-    'restore' {
-        dotnet restore
-    }
-    'build' {
-        dotnet build -c $Config
-    }
-    'test' {
-        dotnet test -c $Config && &{
-            if($ShowCoverageReport) {
-                dotnet tool restore &&
-
-                dotnet tool run reportgenerator `
-                    -reports:**\coverage.cobertura.xml `
-                    -targetdir:.coverage &&
-
-                Start-Process '.coverage\index.htm'
-            }
+try {
+    switch($Target) {
+        'clean' {
+            git clean -dfx -e .vs -e .vscode
+        }
+        'restore' {
+            dotnet restore
+        }
+        'build' {
+            dotnet build -c $Config
+        }
+        'test' {
+            dotnet test -c $Config
+        }
+        'coverage' {
+            dotnet tool restore &&
+            dotnet tool run reportgenerator -reports:**\coverage.cobertura.xml `
+                -targetdir:.coverage && Start-Process '.coverage\index.htm'
         }
     }
-    'pack' {
-        dotnet test -c $Config &&
-        dotnet pack -c $Config -o '.build' --no-build
-    }
-    'run' {
-        dotnet run -c $Config -p $StartProject --launch-profile $LaunchProfile
-    }
-    'watch' {
-        dotnet watch -p $StartProject run -c $Config --launch-profile $LaunchProfile
-    }
 }
-
-Pop-Location
+finally {
+    Pop-Location
+}
