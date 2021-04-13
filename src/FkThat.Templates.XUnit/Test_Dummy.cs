8da0b1bb8f2cc7df4c72eaf9c0e079ddf74160bf
FkThat <fkthat@yandex.ru>
2021-04-13 15:54:07 +0300

    Add dummy classes

---
+++ src/FkThat.Templates.XUnit/Test_Dummy.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FakeItEasy;
+using FluentAssertions;
+using Xunit;
+
+namespace __TargetProject
+{
+    public class Test_Dummy
+    {
+        [Fact]
+        public void Dummy()
+        {
+            // Always pass
+        }
+    }
+}
