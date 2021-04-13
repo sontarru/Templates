3842079281dd2e008d51379331c387ca34fe0f40
FkThat <fkthat@yandex.ru>
2021-04-13 17:20:50 +0300

    Update tools.ps1

--- src/FkThat.Templates.Solution/tools.ps1
+++ src/FkThat.Templates.Solution/tools.ps1
@@ -1,7 +1,7 @@
 [CmdletBinding()]
 param(
     [Parameter(Position = 0, Mandatory = $true)]
-    [ValidateSet('start-feature', 'finish-feature')]
+    [ValidateSet('start-feature', 'finish-feature', 'new-project')]
     $Command,
     [Parameter(ValueFromRemainingArguments = $true)]
     [string[]]
@@ -13,20 +13,24 @@ Push-Location $PSScriptRoot
 try {
     switch($Command) {
         'start-feature' {
+            $name = $Args[0]
             git checkout develop && `
-            git checkout -b feature/$Args[0]
+            git checkout -b feature/$name && `
+            git push -u origin feature/$name
         }
         'finish-feature' {
             git checkout develop && `
             git pull && `
             git remote prune origin && `
-            git branch -d feature/$Args[0]
+            git branch -d feature/$($Args[0])
         }
         'new-project' {
-            dotnet new ft-$Args[0] -o src\$Args[1] && `
-            dotnet new ft-xunit -o test\Test.$Args[1] -tp $Args[1] && `
-            dotnet sln add src\$Args[1]\$Args[1].csproj && `
-            dotnet sln add test\Test.$Args[1]\Test.$Args[1].csproj
+            $templ = $Args[0]
+            $name = $Args[1]
+            dotnet new ft-$templ -o src\$name && `
+            dotnet new ft-xunit -o test\Test.$name -tp $name && `
+            dotnet sln add src\$name\$($name).csproj && `
+            dotnet sln add test\Test.$name\Test.$($name).csproj
         }
     }
 }
