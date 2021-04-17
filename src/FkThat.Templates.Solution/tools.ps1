54f797ec979e69912b61a34d37f3edacb04795a1
FkThat <fkthat@yandex.ru>
2021-04-17 08:27:35 +0300

    Update solution template

--- src/FkThat.Templates.Solution/tools.ps1
+++ src/FkThat.Templates.Solution/tools.ps1
@@ -1,21 +1,28 @@
 [CmdletBinding()]
 param(
+    # the command
     [Parameter(Position = 0, Mandatory = $true)]
     [ValidateSet('new-project')]
     $Command,
+    # the command arguments
     [Parameter(ValueFromRemainingArguments = $true)]
-    [string[]]
-    $Args
+    [string[]] $Arguments
 )
+
+# if no arguments passed
+if(-not $Arguments) {
+    $Arguments = @()
+}
 
 Push-Location $PSScriptRoot
 
 try {
     switch($Command) {
+        # create the project by the template along with the test project
         'new-project' {
-            $templ = $Args[0]
-            $name = $Args[1]
-            dotnet new ft-$templ -o src\$name && `
+            $template = $Arguments[0]
+            $name = $Arguments[1]
+            dotnet new ft-$template -o src\$name && `
             dotnet new ft-xunit -o test\Test.$name -tp $name && `
             dotnet sln add src\$name\$($name).csproj && `
             dotnet sln add test\Test.$name\Test.$($name).csproj
@@ -25,4 +32,3 @@ try {
 finally {
     Pop-Location
 }
-
