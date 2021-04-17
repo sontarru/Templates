5c28eb9c1a266c18ec70b23e8867246dea51e953
FkThat <fkthat@yandex.ru>
2021-04-17 20:30:40 +0300

    Add item template

---
+++ src/FkThat.Templates.Item/Item.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+#if isxunit
+using FakeItEasy;
+using FluentAssertions;
+using Xunit;
+#endif
+
+namespace @namespace
+{
+#if (isclass || isxunit)
+    public class Item
+    {
+    }
+#elif isinterface
+    public interface Item
+    {
+    }
+#endif
+}
