1fd5fb02a19c900bbb9b4ace533d13715688f624
FkThat <fkthat@yandex.ru>
2021-04-18 08:39:43 +0300

    Add build.ps1

---
+++ build.ps1
@@ -0,0 +1,39 @@
+[CmdletBinding()]
+param(
+    [Parameter(Position = 0)]
+    [ValidateSet('clean', 'restore', 'build', 'test', 'coverage', 'pack')]
+    $Target = 'build',
+    [Parameter()]
+    [ValidateSet('Debug', 'Release')]
+    $Config = 'Debug'
+)
+
+Push-Location $PSScriptRoot
+
+try {
+    switch($Target) {
+        'clean' {
+            git clean -dfx -e .vs -e .vscode
+        }
+        'restore' {
+            dotnet restore
+        }
+        'build' {
+            dotnet build -c $Config
+        }
+        'test' {
+            dotnet test -c $Config -l trx
+        }
+        'coverage' {
+            dotnet tool restore &&
+            dotnet tool run reportgenerator -reports:**\coverage.cobertura.xml `
+                -targetdir:.coverage && Start-Process '.coverage\index.htm'
+        }
+        'pack' {
+            dotnet pack -c $Config -o '.build'
+        }
+    }
+}
+finally {
+    Pop-Location
+}
