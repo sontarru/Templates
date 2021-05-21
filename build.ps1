69cbd940087e395d249761121c41992cae5e6348
fkthat <fkthat@yandex.ru>
2021-05-21 23:42:48 +0300

    Update build.ps1 script

--- build.ps1
+++ build.ps1
@@ -5,26 +5,22 @@ param(
     $Target = 'build'
 )
 
-Push-Location $PSScriptRoot
+switch($Target) {
+    'clean' {
+        Push-Location $PSScriptRoot
+        git clean -dfx -e .vs -e .vscode
+        Pop-Location
+    }
+    'restore' {
+        Get-ChildItem "$PSScriptRoot\src" -Recurse -Filter *.csproj |
+            ForEach-Object { dotnet restore $_ }
+    }
+    'install' {
+        Get-ChildItem "$PSScriptRoot\src" |
+            Where-Object { Test-Path "$_\.template.config" } |
+            ForEach-Object { dotnet new -i $_ | Out-Null }
 
-try {
-    switch($Target) {
-        'clean' {
-            git clean -dfx -e .vs -e .vscode
-        }
-        'restore' {
-            Get-ChildItem "$PSScriptRoot\src" -Recurse -Filter *.csproj |
-                ForEach-Object { dotnet restore $_ }
-        }
-        'install' {
-            Get-ChildItem "$PSScriptRoot\src" |
-                Where-Object { Test-Path "$_\.template.config" } |
-                ForEach-Object { dotnet new -i $_ | Out-Null }
-
-            dotnet new -l | Select-String '^FkThat' -NoEmphasis
-        }
+        dotnet new -l | Select-String '^FkThat' -NoEmphasis
     }
 }
-finally {
-    Pop-Location
-}
+
