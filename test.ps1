397ebc1e5c3c14b615304daa04aac71ffcdc4f0b
fkthat <fkthat@yandex.ru>
2021-12-27 01:51:11 +0300

    Add test.ps1

---
+++ test.ps1
@@ -0,0 +1,13 @@
+$dotnet = "$env:ProgramFiles\dotnet\dotnet.exe"
+$name = "Foo.Bar"
+
+& $dotnet new ft-sln -o .test\${name}
+& $dotnet new ft-classlib -o .test\${name}\src\${name}.ClassLib
+& $dotnet new ft-console -o .test\${name}\src\${name}.ConsoleApp
+& $dotnet new ft-webapi -o .test\${name}\src\${name}.WebApi
+& $dotnet new ft-worker -o .test\${name}\src\${name}.Worker
+& $dotnet new ft-xunit -o .test\${name}\test\UnitTests.${name}.ClassLib
+& $dotnet new ft-xunit -o .test\${name}\test\UnitTests.${name}.ConsoleApp
+& $dotnet new ft-xunit -o .test\${name}\test\UnitTests.${name}.WebApi
+& $dotnet new ft-xunit -o .test\${name}\test\UnitTests.${name}.Worker
+& $dotnet build .test\${name}\${name}.sln
