9a147e48e30bf9bb52601c5332ce4e4e1816c469
fkthat <fkthat@yandex.ru>
2021-12-27 01:59:38 +0300

    Fix XML comments in the worker template

--- templates/FkThat.Templates.Worker/Worker.cs
+++ templates/FkThat.Templates.Worker/Worker.cs
@@ -1,14 +1,31 @@
 namespace FkThat.Templates.Worker;
 
+/// <summary>
+/// Sample background worker.
+/// </summary>
 public class Worker : BackgroundService
 {
     private readonly ILogger<Worker> _logger;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Worker"/> class.
+    /// </summary>
+    /// <param name="logger">The logger.</param>
     public Worker(ILogger<Worker> logger)
     {
         _logger = logger;
     }
 
+    /// <summary>
+    /// This method is called when the <see cref="T:Microsoft.Extensions.Hosting.IHostedService"/>
+    /// starts. The implementation should return a task that represents the lifetime of the long
+    /// running operation(s) being performed.
+    /// </summary>
+    /// <param name="stoppingToken">
+    /// Triggered when <see
+    /// cref="M:Microsoft.Extensions.Hosting.IHostedService.StopAsync(System.Threading.CancellationToken)"/>
+    /// is called.
+    /// </param>
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         while (!stoppingToken.IsCancellationRequested)
