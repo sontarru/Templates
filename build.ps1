5c64a8efc246740765e8ee8f46c1c50c2311aff0
fkthat <me@fkthat.net>
2022-11-25 16:09:40 +0300

    "dotnet pack" fixed

--- build.ps1
+++ build.ps1
@@ -1,2 +1,2 @@
 $dotnet = "$env:ProgramFiles\dotnet\dotnet.exe"
-& $dotnet pack .\FkThat.Templates.csproj -o .\.Build --no-build
+& $dotnet pack .\FkThat.Templates.csproj -o .\.build -p:Version=0.0.42
