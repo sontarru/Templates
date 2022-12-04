714a27595937eaf569c25e403d3061ab080eb0fd
fkthat <fkthat@yandex.ru>
2022-12-04 07:03:19 +0300

    Update the Worker template.

--- templates/FkThat.Templates.Worker/Worker.cs
+++ templates/FkThat.Templates.Worker/Worker.cs
@@ -1,5 +1,3 @@
-using FkThat.Templates.Worker.Extensions;
-
 namespace FkThat.Templates.Worker;
 
 /// <summary>
@@ -19,21 +17,19 @@ public class Worker : BackgroundService
     }
 
     /// <summary>
-    /// This method is called when the <see cref="IHostedService"/>
-    /// starts. The implementation should return a task that represents the lifetime of the long
-    /// running operation(s) being performed.
+    /// This method is called when the <see cref="IHostedService"/> starts. The implementation
+    /// should return a task that represents the lifetime of the long running operation(s) being performed.
     /// </summary>
     /// <param name="stoppingToken">
-    /// Triggered when <see
-    /// cref="IHostedService.StopAsync(CancellationToken)"/>
-    /// is called.
+    /// Triggered when <see cref="IHostedService.StopAsync(CancellationToken)"/> is called.
     /// </param>
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        while (!stoppingToken.IsCancellationRequested)
+        while (true)
         {
-            _logger.WorkerRunning(DateTimeOffset.Now);
-            await Task.Delay(1000, stoppingToken).ConfigureAwait(false);
+            stoppingToken.ThrowIfCancellationRequested();
+            _logger.LogInformation("The worker is running.");
+            await Task.Delay(TimeSpan.FromSeconds(3), stoppingToken);
         }
     }
 }
