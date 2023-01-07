95ae0bd390208cdae904a46af4a4af48a7ff2ae7
fkthat <fkthat@yandex.ru>
2023-01-07 21:31:08 +0300

    ConsoleApp tests.

--- src/FkThat.Templates.ConsoleApp/HelloCmd.cs
+++ src/FkThat.Templates.ConsoleApp/HelloCmd.cs
@@ -1,5 +1,7 @@
 using System.Diagnostics.CodeAnalysis;
 
+using FkThat.Gists;
+
 namespace FkThat.Templates.ConsoleApp;
 
 /// <summary>
@@ -7,10 +9,27 @@ namespace FkThat.Templates.ConsoleApp;
 /// </summary>
 public class HelloCmd : ICommand<string>
 {
+    private readonly ISystemConsole _console;
+
+    /// <summary>
+    /// Initialize <c cref="HelloCmd"/> instance.
+    /// </summary>
+    public HelloCmd(ISystemConsole console)
+    {
+        _console = console ?? throw new ArgumentNullException(nameof(console));
+    }
+
     /// <inheritdoc/>
     [SuppressMessage("Naming", "CA1725:Parameter names should match base declaration")]
     public async Task ExecAsync(string who, CancellationToken cancellationToken)
     {
-        await Console.Out.WriteLineAsync($"Hello, {who}!").ConfigureAwait(false);
+        _ = who ?? throw new ArgumentNullException(nameof(who));
+
+        if (string.IsNullOrWhiteSpace(who))
+        {
+            throw new ArgumentException($"{nameof(who)} must not be blank.", nameof(who));
+        }
+
+        await _console.Out.WriteLineAsync($"Hello, {who}!").ConfigureAwait(false);
     }
 }
---
+++ test/UnitTests.FkThat.Templates.ConsoleApp/Test_HelloCmd.cs
@@ -0,0 +1,52 @@
+﻿using FkThat.Gists;
+
+namespace FkThat.Templates.ConsoleApp;
+
+public class Test_HelloCmd
+{
+    [Fact]
+    public void Ctor_should_check_null_console()
+    {
+        ISystemConsole console = null!;
+        FluentActions.Invoking(() => new HelloCmd(console))
+            .Should().Throw<ArgumentNullException>()
+            .Which.ParamName.Should().Be(nameof(console));
+    }
+
+    [Fact]
+    public async Task ExecAsync_should_check_null_who()
+    {
+        string who = null!;
+        HelloCmd sut = new(A.Fake<ISystemConsole>());
+
+        await sut.Invoking(s => s.ExecAsync(who, default))
+            .Should().ThrowAsync<ArgumentNullException>()
+            .WithParameterName(nameof(who));
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData(" ")]
+    public async Task ExecAsync_should_check_blank_who(string who)
+    {
+        var console = A.Fake<ISystemConsole>();
+        StringWriter stdout = new();
+        A.CallTo(() => console.Out).Returns(stdout);
+        HelloCmd sut = new(console);
+
+        await sut.Invoking(s => s.ExecAsync(who, default))
+            .Should().ThrowAsync<ArgumentException>()
+            .WithParameterName(nameof(who));
+    }
+
+    [Fact]
+    public async Task ExecAsync_should_say_hello()
+    {
+        var console = A.Fake<ISystemConsole>();
+        StringWriter stdout = new();
+        A.CallTo(() => console.Out).Returns(stdout);
+        HelloCmd sut = new(console);
+        await sut.ExecAsync("World", default).ConfigureAwait(false);
+        stdout.ToString().Should().Be($"Hello, World!{Environment.NewLine}");
+    }
+}
